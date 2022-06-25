1c0957f095b1678b55d4634dece66bdb5f6ad647
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-25 14:59:51 +0100

    added items on  and navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import {Routes, Route} from "react-router-dom"
 import {Container} from "react-bootstrap"
 import {Home} from "./pages/Home"
+import { About } from "./pages/About"
+import { Store } from "./pages/Store"
+import {Navbar} from "./components/Navbar"
+
+
+
 
 
 function App() {
   
 
-  return (<Container>
+  return (
+    <>
+    <Navbar />
+  <Container className="mb-4">
     <Routes>
 
       <Route path = "/" element={<Home />}/>
@@ -15,6 +24,7 @@ function App() {
     </Routes>
 
   </Container>
+  </>
   )
 }
 
